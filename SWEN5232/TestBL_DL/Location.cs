0a0d2141f421198d9fa270a7772adecc9ba7c266
ChandraMouliBasava <38330496+ChandraMouliBasava@users.noreply.github.com>
2019-04-14 18:58:40 -0500

    Add files via upload

---
+++ SWEN5232/TestBL_DL/Location.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWEN5232.TestBL_DL
+{
+    public class Location
+    {
+
+        public Location(string lat, string longi, string alti)
+        {
+            latitude = lat;
+            longitude = longi;
+            altitude = alti;
+        }
+
+        public Location(string lat, string longi, string alti, string address)
+        {
+            latitude = lat;
+            longitude = longi;
+            altitude = alti;
+            this.address = address;
+        }
+
+        public Location() { }
+
+        private string latitude;
+        private string longitude;
+        private string altitude;
+        private string address;
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public string Altitude
+        {
+            get { return altitude; }
+            set { altitude = value; }
+        }
+
+        public string Longitude
+        {
+            get { return longitude; }
+            set { longitude = value; }
+        }
+
+        public string Latitude
+        {
+            get { return latitude; }
+            set { latitude = value; }
+        }
+
+    }
+}
---
+++ SWEN5232/TestBL_DL/Project.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWEN5232.TestBL_DL
+{
+    public class Project
+    {
+        public string ProjectID { get; set; }
+
+        
+        private string projectName;
+        
+        private int floodLevel;
+    
+        private Model building;
+       
+        private Location location;
+
+        public Location Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+        public Model Building
+        {
+            get { return building; }
+            set { building = value; }
+        }
+        public string ProjectName
+        {
+            get { return projectName; }
+            set { projectName = value; }
+        }
+        public int FloodLevel
+        {
+            get { return floodLevel; }
+            set { floodLevel = value; }
+        }
+    }
+}
+
+    
